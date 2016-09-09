20f1b3181ce80c1c98e9d62d280ac1b0189084db
Gabriele Soldati <soldati.gabriele@gmail.com>
2016-09-09 21:05:12 +0100

    imagevault index finisched

--- Works/ImageVault/index.php
+++ Works/ImageVault/index.php
@@ -2,46 +2,43 @@
 require_once('inc/init.php');
 
 if (!$user->isLoggedIn()) {
-    $user = new User('admin');
+    $user = new User('Gabriele');
     $user->login();
     Redirect::to('index.php');
 }
 require_once('navigation.php');
 if ($user->isLoggedIn()) {
 ?>
-<div class="homeContainer" style="margin-bottom:1%; height:100%;">
-    <div class="col-md-8 homeFrame">
+    <div  style="width:450px;margin:auto;">
 
-        <div class="clearfix"></div>
-        <div class="col-md-6">
-
-            <!-- il nome dell'utente viene ritornato tramite la funzione escape, per prevenire attachi di tipo cross site scripting  -->
-            <div class="col-md-12 spacing">
-                <h3>Welcome, <a
-                        href="profile.php?username=<?php echo escape($user->data()->username); ?>"><?php echo escape($user->data()->username); ?></a>!
-                </h3>
+        <div class="col-md-12 offset-md-2">
+            <h3>Welcome, <?php echo escape($user->data()->username); ?>!</h3>
+        </div>
+        <div class="col-md-12" style="border:1px solid #acacac;">
+            <div class="clearfix"><br></div>
+            <div class="offset-md-3">
+                <img src="img/images.png" alt="" style="max-width:220px;">
             </div>
+            <div class="clearfix"><br></div>
             <div class="col-md-12">
-                <form action="upload.php" method="POST"><input type="submit"
-                                                               class="showImage btn btn-block  btn-outline-primary"
-                                                               name="Upload" value="Upload Images"></form>
-            </div>
-            <div class="col-md-12 clearfix"><br></div>
-            <div class="col-md-12">
-                <form action="../../index.php" method="POST"><input type="submit"
-                                                                    class="showImage btn btn-block btn-outline-primary"
-                                                                    name="" value="Back to the Portfolio"></form>
+                <div class="col-md-12">
+                    <form action="upload.php" method="POST">
+                        <input type="submit"
+                               class="showImage btn btn-block btn-outline-primary"
+                               name="Upload" value="Upload Images">
+                    </form>
+                    <div class="clearfix"><br></div>
+                </div>
             </div>
         </div>
-        <?php
+        <div class="col-md-12 clearfix"><br></div>
+        <div class="col-md-8 offset-md-2">
+            <form action="../../index.php" method="POST"><input type="submit"
+                                                                class="showImage btn btn-block btn-outline-gray"
+                                                                name="" value="Back to the Portfolio"></form>
+        </div>
 
-        }
-        if ($_GET["password"] == "changed") {
-            echo '<script type="text/javascript">alert("password modificata correttamente"); </script>';
-        }
-        ?>
-
-    </div>
+        <?php } ?>
 </div>
 </body>
 </html>
